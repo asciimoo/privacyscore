a08ada10222a2343e14e7cd73791eeb234d2d4b7
Adam Tauber <asciimoo@gmail.com>
2016-03-14 20:44:02 +0100

    [enh] secure server headers

--- server/server.go
+++ server/server.go
@@ -31,6 +31,8 @@ func Run(listen *string) error {
 }
 
 func requestRouter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("X-Xss-Protection", "1; mode=block")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
 	switch r.URL.Path {
 	case "/":
 		serveIndexPage(w, r)
