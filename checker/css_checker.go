558a6e12fcd450c88af1cc8e4f45a6e0fcede218
Adam Tauber <asciimoo@gmail.com>
2016-03-29 00:43:09 +0200

    [enh] more efficient css checking

--- checker/css_checker.go
+++ checker/css_checker.go
@@ -1,11 +1,17 @@
 package checker
 
 import (
-	"fmt"
+	"bytes"
+	"net/url"
+	"regexp"
 	"strings"
 
-	"github.com/asciimoo/css/scanner"
+	"github.com/asciimoo/privacyscore/penalty"
+	"github.com/asciimoo/privacyscore/utils"
 )
+
+// TODO
+var URLREGEXP *regexp.Regexp = regexp.MustCompile("url\\(['\"]([\u0009\u0021\u0023-\u0026\u0028\u002a-\u007E]+)['\"]\\)")
 
 type CSSChecker struct{}
 
@@ -13,34 +19,27 @@ func (_ *CSSChecker) Check(c *CheckJob, p *PageInfo) {
 	if !strings.Contains(strings.ToLower(p.ContentType), "css") {
 		return
 	}
-	s := scanner.New(string(p.ResponseBody))
-	for {
-		t := s.Next()
-		switch t.Type {
-		case scanner.TokenEOF:
-			return
-		case scanner.TokenURI:
-			if strings.HasPrefix(t.Value, "data:") {
-				break
-			}
-			var url string
-			if strings.HasPrefix(t.Value, "url(") {
-				if len(t.Value) < 4 {
-					break
-				}
-				switch t.Value[4] {
-				case byte('"'), byte('\''):
-					url = t.Value[5 : len(t.Value)-2]
-				default:
-					url = t.Value[4 : len(t.Value)-1]
-				}
-			} else {
-				url = t.Value
-			}
-			fmt.Println(url)
-		case scanner.TokenIncludes:
-			// TODO
+	for _, b := range URLREGEXP.FindAll(p.ResponseBody, 128) {
+		if len(b) < 6 {
 			break
+		}
+		switch b[4] {
+		case byte('"'), byte('\''):
+			b = b[5 : len(b)-2]
+		default:
+			b = b[4 : len(b)-1]
+		}
+		if bytes.HasPrefix(b, []byte("data:")) {
+			break
+		}
+		u, err := url.Parse(string(b))
+		if err != nil {
+			break
+		}
+		if utils.IsForeignHost(u.Host, p.Domain) {
+			c.Result.Penalties.Add(penalty.P_EXTERNAL_RESOURCE, utils.CropSubdomains(u.Host))
+		} else {
+			c.CheckURL(utils.GetFullURL(u, p.URL))
 		}
 	}
 }
