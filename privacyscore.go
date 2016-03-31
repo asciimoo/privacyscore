cbb08c994f5142a1dee3c30d6d719d8b7cfb76c4
Adam Tauber <asciimoo@gmail.com>
2016-03-31 18:13:31 +0200

    [enh] command line checker

--- privacyscore.go
+++ privacyscore.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
+	"github.com/asciimoo/privacyscore/checker"
 	"github.com/asciimoo/privacyscore/server"
 )
 
@@ -13,5 +15,12 @@ func main() {
 	flag.Parse()
 	if len(flag.Args()) == 0 {
 		server.Run(listen)
+	} else {
+		c, err := checker.Run(flag.Args()[0])
+		if err == nil {
+			fmt.Println(c.Result.Penalties.GetScore())
+		} else {
+			fmt.Println("Something went wrong:", err)
+		}
 	}
 }
