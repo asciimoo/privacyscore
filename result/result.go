4dde849a406879906aae2e92bd367b67e29d7d78
Adam Tauber <asciimoo@gmail.com>
2016-04-08 20:21:36 +0200

    [mod] deduplicate errors

--- result/result.go
+++ result/result.go
@@ -22,6 +22,11 @@ func New(URL string) *Result {
 }
 
 func (r *Result) AddError(e error) {
+	for _, e2 := range r.Errors {
+		if e.Error() == e2.Error() {
+			return
+		}
+	}
 	r.Lock()
 	r.Errors = append(r.Errors, e)
 	r.Unlock()
